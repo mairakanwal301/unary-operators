871fc35d143e31efab818c24d81eb5a7da190cf6
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-18 02:01:13 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+//let index = 10 ;
+//let result = ++index + ++index + 8 + --index + -2 ;
+// 11     + 12     + 8  +  11 - 2
+//console.log(result) ;
+let a = 5;
+let b = 2;
+let c = ++a + a++ + --b + b-- + a + b++ + b;
+//   
+console.log(c);
+// ++a + a++ + --b + b-- + a + b++ +b ;
---
+++ app.ts
@@ -0,0 +1,19 @@
+//let index = 10 ;
+
+//let result = ++index + ++index + 8 + --index + -2 ;
+            // 11     + 12     + 8  +  11 - 2
+//console.log(result) ;
+
+
+
+let a:number = 5 ;
+let b:number = 2 ;
+let c:number = ++a + a++ + --b + b-- + a + b++ +b ;
+          //   
+
+console.log(c) ;
+
+
+
+// ++a + a++ + --b + b-- + a + b++ +b ;
+ 
